e1d7643bc2baade0f2e40d5f1daee39fc8c045d6
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2019-01-15 10:25:27 -1000

    test file setup

--- js/pig-latin.js
+++ js/pig-latin.js
@@ -2,6 +2,7 @@
 
 const pigLatinator = function () {
   const vowels = ['a', 'e', 'i', 'o', 'u'];
+  const pigLatinResult = [];
 
   const toPigLatin = (str) => {
     if (!str || typeof str !== 'string') {
@@ -20,9 +21,10 @@ const pigLatinator = function () {
       }
     })
     .map(word => {
-      return `${word}-ay`;
+      const converted = `${word}-ay`;
+      pigLatinResult.push(converted);
+      return converted;
     })
-    .join(' ');
 
     const beginsWithConsonant = words
     .filter(word => {
@@ -47,22 +49,30 @@ const pigLatinator = function () {
       if (spliceIndex > 0) {
         const untilFirstVowel = lowerCase.slice(0, spliceIndex);
         const removeFirstLetter = lowerCase.substring(spliceIndex);
+        const converted = `${removeFirstLetter}-${untilFirstVowel}ay`;
 
-        return `${removeFirstLetter}-${untilFirstVowel}ay`;
+        pigLatinResult.push(converted);
+        return converted;
       }
 
       return lowerCase;
     })
-    .join(' ');
 
-    console.log(beginsWithVowel);
-    console.log(beginsWithConsonant);
+    // console.log(beginsWithVowel);
+    // console.log('\n\n');
+    // console.log(beginsWithConsonant);
 
     return result.join(' ');
   }
 
+  const getPigLatinResult = () => {
+    console.log(pigLatinResult);
+    return pigLatinResult.join(' ');
+  }
+
   return {
     toPigLatin : toPigLatin,
+    getPigLatinResult : getPigLatinResult,
     // toEnglish : toEnglish
   }
 }
@@ -70,3 +80,4 @@ const pigLatinator = function () {
 const sampleText = `Rain dance trust the process kirlian photography, combined energy fields burning man. Astrological earth day tantra, sarong loving kindness alkaline healing energy fractal nature of. Surrender didgeridoo tie-dye, the dali lama acupuncture Hafiz salvia cold plunge hooping. Reproductive system lingham contact improv, dharma stevia twelfth house seventh generation new paradigm apple cider vinegar solstice talking stick. Ecstatic dance rebirthing mind-body science has proven that papasan tai chi authentic self cleansing, human potential yogic. Biodiesel midwifery nonlinear, marinese perineum heart-based heartbeat of our ancestors kombucha sacred ruins dead sea minerals. Himalayan sea salt medical marijuana taurus hemp seed optimal frequency, shanti nag champa. Namaste.`
 
 pigLatinator().toPigLatin(sampleText);
+pigLatinator().getPigLatinResult();
---
+++ test/pig-latin-spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const pigLatinModule = require('../js/pig-latin.js');
+const chai = require('chai');
+const should = chai.should();
+
+describe('Pig Latin Converter', function () {
+  it('ljadsladskjadslkjasdljadskdsa', function () {
+    
+  });
+});
